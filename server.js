998995935f08604f31c0dee2161d94bf533e5e5d
bryan <bryan.lee.jh@gmail.com>
2020-04-19 03:14:26 +0800

    add csp

--- server.js
+++ server.js
@@ -12,7 +12,15 @@ app.use(helmet());
 app.use(bodyParser.json());
 // app.use(morgan("combined"))s;
 
-app.get("/", (req, res) => res.send("Hello World!"));
+app.get("/", (req, res) => {
+  res.cookie('session', '1', {httpOnly: true});
+  res.cookie('session', '1', {secure: true});
+  res.set({
+    'Content-Security-Policy': 
+    "script-src 'self' 'https://apis.google.com'"
+  })
+  res.send("Hello World!");
+});
 
 app.post("/secret", (req, res) => {
   const { userInput } = req.body;
